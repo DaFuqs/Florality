dfdb1f5d3c116ce38c848b290d54aeaae50518e7
DaFuqs <dominik.m.schmidt@googlemail.com>
2023-06-19 16:56:00 +0200

    Initial commit

---
+++ src/main/java/me/sria/srias_flowers/SriasFlowers.java
@@ -0,0 +1,13 @@
+package me.sria.srias_flowers;
+
+import net.fabricmc.api.*;
+
+public class SriasFlowers implements ModInitializer {
+	/**
+	 * Runs the mod initializer.
+	 */
+	@Override
+	public void onInitialize() {
+	
+	}
+}
---
+++ src/main/java/me/sria/srias_flowers/client/SriasFlowersClient.java
@@ -0,0 +1,13 @@
+package me.sria.srias_flowers.client;
+
+import net.fabricmc.api.*;
+
+public class SriasFlowersClient implements ClientModInitializer {
+	/**
+	 * Runs the mod initializer on the client environment.
+	 */
+	@Override
+	public void onInitializeClient() {
+	
+	}
+}
